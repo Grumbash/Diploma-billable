c921bd287ff1d36049df38e33dcfc11b9ce0a216
Rossev Gleb <rossevgleb@gmail.com>
2019-05-21 00:41:47 +0300

    Added functionality

--- client/js/script.js
+++ client/js/script.js
@@ -2,41 +2,49 @@ let uploadName;
 const sections = $(".upload-section");
 const url = "http://localhost:3000";
 const data = new FormData();
+
 $(".detect").on("click", function(e) {
   uploadName = "detect";
-  $(".upload-section-first").toggleClass("upload-section_active");
+  $(".upload-section").removeClass("upload-section_active");
+  $(".upload-section-detect").toggleClass("upload-section_active");
 });
 
 $(".parse").on("click", function(e) {
   uploadName = "parse";
-  $(".upload-section-first").toggleClass("upload-section_active");
+  $(".upload-section").removeClass("upload-section_active");
+  $(".upload-section-parse").toggleClass("upload-section_active");
 });
 
 $(".convert").on("click", function(e) {
   uploadName = "convert";
-  $(".upload-section-first").toggleClass("upload-section_active");
+  $(".upload-section").removeClass("upload-section_active");
+  $(".upload-section-convert").toggleClass("upload-section_active");
 });
 
 $(".time").on("click", function(e) {
   uploadName = "time";
-  $(".upload-section-second").toggleClass("upload-section_active");
+  $(".upload-section").removeClass("upload-section_active");
+  $(".upload-section-time").toggleClass("upload-section_active");
 });
 
 $(".resync").on("click", function(e) {
   uploadName = "resync";
-  $(".upload-section-second").toggleClass("upload-section_active");
+  $(".upload-section").removeClass("upload-section_active");
+  $(".upload-section-resync").toggleClass("upload-section_active");
 });
 
 $(".convertToFormat").on("click", function(e) {
   uploadName = "convertToFormat";
-  $(".upload-section-second").toggleClass("upload-section_active");
+  $(".upload-section").removeClass("upload-section_active");
+  $(".upload-section-convertToFormat").toggleClass("upload-section_active");
 });
 
-$(".upload-section-first__btn").on("click", function(e) {
-  const files = $(".upload-section-first__input")[0].files;
+$(".upload-section-detect__btn").on("click", function(e) {
+  e.preventDefault();
+  const files = $(".upload-section-detect__input")[0].files;
   const data_inner = files[files.length - 1];
   console.log("TCL: data_inner", files);
-  data.append(data_inner.name, data_inner);
+  data.set(data_inner.name, data_inner);
   $.ajax({
     url: url + "/" + uploadName,
     data: data,
@@ -48,7 +56,104 @@ $(".upload-section-first__btn").on("click", function(e) {
       console.log(res);
     }
   });
-  data.delete(data_inner.name);
+});
 
-  // console.log(data_inner);
+$(".upload-section-parse__btn").on("click", function(e) {
+  e.preventDefault();
+  const files = $(".upload-section-parse__input")[0].files;
+  const data_inner = files[files.length - 1];
+  console.log("TCL: data_inner", files);
+  data.set(data_inner.name, data_inner);
+  $.ajax({
+    url: url + "/" + uploadName,
+    data: data,
+    cache: false,
+    contentType: false,
+    processData: false,
+    method: "POST",
+    success: function(res) {
+      console.log(res);
+    }
+  });
+});
+
+$(".upload-section-convert__btn").on("click", function(e) {
+  e.preventDefault();
+  const files = $(".upload-section-convert__input")[0].files;
+  const formatType = $("#format-select")[0].value;
+  const data_inner = files[files.length - 1];
+
+  data.set(data_inner.name, data_inner);
+  data.set("formatType", formatType);
+  console.log("TCL: data", data.get("formatType"));
+
+  $.ajax({
+    url: url + "/" + uploadName,
+    data: data,
+    cache: false,
+    contentType: false,
+    processData: false,
+    method: "POST",
+    success: function(res) {
+      console.log(res);
+    }
+  });
+});
+
+$(".upload-section-time__bnt").on("click", function(e) {
+  e.preventDefault();
+  const files = $(".upload-section-time__input")[0].files;
+  const offset = $("#time-number")[0].value;
+  const data_inner = files[files.length - 1];
+  data.set(data_inner.name, data_inner);
+  data.set("offset", offset);
+  $.ajax({
+    url: url + "/" + uploadName,
+    data: data,
+    cache: false,
+    contentType: false,
+    processData: false,
+    method: "POST",
+    success: function(res) {
+      console.log(res);
+    }
+  });
+});
+
+$(".upload-section-resync__btn").on("click", function(e) {
+  e.preventDefault();
+  const files = $(".upload-section-resync__input")[0].files;
+  const data_inner = files[files.length - 1];
+  console.log("TCL: data_inner", files);
+  data.set(data_inner.name, data_inner);
+  $.ajax({
+    url: url + "/" + uploadName,
+    data: data,
+    cache: false,
+    contentType: false,
+    processData: false,
+    method: "POST",
+    success: function(res) {
+      console.log(res);
+    }
+  });
+});
+
+$(".upload-section-convertToFormat__btn").on("click", function(e) {
+  e.preventDefault();
+  const files = $(".upload-section-convertToFormat__input")[0].files;
+  const data_inner = files[files.length - 1];
+  console.log("TCL: data_inner", files);
+  data.set(data_inner.name, data_inner);
+  $.ajax({
+    url: url + "/" + uploadName,
+    data: data,
+    cache: false,
+    contentType: false,
+    processData: false,
+    method: "POST",
+    success: function(res) {
+      console.log(res);
+    }
+  });
 });
--- index.js
+++ index.js
@@ -1,74 +1,36 @@
 const express = require("express");
 const app = express();
 const http = require("http").createServer(app);
-const subsrt = require("subsrt");
 const fileUpload = require("express-fileupload");
-const io = require("socket.io")(http);
-const fs = require("fs");
+const docx = require("docx");
+const middleware = require("./middleware.js");
 
 app.use(fileUpload());
 
 app.use(express.static("client"));
 
-app.post("/upload", (req, res) => {
-  console.log(req);
-});
-
-app.post("/convert", (req, res) => {
-  console.log(req);
-});
-
 app.post("/detect", (req, res) => {
-  if (Object.keys(req.files).length == 0) {
-    return res.status(400).send("No files were uploaded.");
-  }
-  const name = Object.keys(req.files)[0];
-  let file = req.files[name];
-  const path = __dirname + "/files/" + name;
-  console.log(file);
-  if (file instanceof Array) {
-    file = file[file.length - 1];
-  }
-
-  file.mv(path, function(err) {
-    if (err) {
-      console.log(err);
-      return res.status(500).send(err);
-    }
-    fs.readFile(path, "utf8", (err, contents) => {
-      const result = subsrt.detect(contents);
-      console.log(result);
-      if (!result || Object.keys(result).length == 0) {
-        return res.json("This file does not meet the format");
-      }
-      if (result) res.json({ format: result });
-    });
-
-    fs.unlink(path, err => {
-      if (err) {
-        console.log(err);
-      }
-    });
-  });
+  middleware(req, res, "detect", null);
 });
 
 app.post("/parse", (req, res) => {
-  console.log(req);
+  middleware(req, res, "parse", null);
+});
+
+app.post("/convert", (req, res) => {
+  middleware(req, res, "convert", { format: req.body.formatType });
+});
+
+app.post("/time", (req, res) => {
+  middleware(req, res, "resync", { offset: req.body.offset }, true);
 });
 
 app.post("/resync", (req, res) => {
   console.log(req);
 });
 
-app.post("/time", (req, res) => {
-  console.log(req);
-});
-
-io.on("connection", function(socket) {
-  console.log("a user connected");
-  socket.on("disconnect", function() {
-    console.log("user disconnected");
-  });
+app.post("/convertToFormat", (req, res)=>{
+  
 });
 
 http.listen(3000, function() {
